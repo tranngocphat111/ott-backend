54a702fe63f83d9553138236fa2c336eb741fa36
DarkThinh <139979897+DarkTzeoi@users.noreply.github.com>
2026-03-28 17:55:35 +0700

    delete all

--- analytics-service/src/main/java/iuh/fit/se/analyticsservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package iuh.fit.se.analyticsservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
